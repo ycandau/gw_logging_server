7e8a361b6426bc66361065fbb0d0501501bc2afd
Yves Candau <ycandau@sfu.ca>
2021-10-31 20:52:42 -0700

    Add database schema and seeds

---
+++ database/seeds/logs.sql
@@ -0,0 +1,5 @@
+INSERT INTO logs (id, method, url, ip, data, date_created)
+VALUES
+(1, 'GET', '/api', 'ip', 'alpha', '2021-10-31'),
+(2, 'GET', '/api', 'ip', 'beta', '2021-10-31'),
+(3, 'GET', '/api', 'ip', 'prime', '2021-10-31'),
