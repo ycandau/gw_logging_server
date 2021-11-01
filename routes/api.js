c158f49ad4e86e41cba3c10233aedd67969ee97d
Yves Candau <ycandau@sfu.ca>
2021-11-01 14:06:25 -0700

    Add error handling for database functions

--- routes/api.js
+++ routes/api.js
@@ -5,22 +5,38 @@
 const express = require('express');
 const router = express.Router();
 
+const handleError = (error) => console.error(`\nERROR: ${error.message}\n`);
+
 module.exports = (db) => {
   router.get('/reset', async (_, res) => {
-    await db.reset();
+    try {
+      await db.reset();
+    } catch (error) {
+      handleError(error);
+    }
     res.redirect('/api');
   });
 
   router.get('/', async (_, res) => {
-    const data = await db.browseLogs();
-    res.json(data);
+    try {
+      const data = await db.browseLogs();
+      res.json(data);
+    } catch (error) {
+      handleError(error);
+      res.status(500).send(error.message);
+    }
   });
 
   router.post('/', async (req, res) => {
-    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
-    const log = { ip, method: 'post', url: '/', ...req.body };
-    await db.addLog(log);
-    res.end();
+    try {
+      const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
+      const log = { ip, method: 'post', url: '/', ...req.body };
+      await db.addLog(log);
+      res.end();
+    } catch (error) {
+      handleError(error);
+      res.status(500).send(error.message);
+    }
   });
 
   return router;
