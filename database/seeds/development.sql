98a8851732f9d2a09ade9f7783433b02d7d0c736
Yves Candau <ycandau@sfu.ca>
2021-11-02 00:56:18 -0700

    Change database schema from date to timestamp

--- database/seeds/development.sql
+++ database/seeds/development.sql
@@ -1,5 +1,5 @@
 INSERT INTO logs (ip, method, url, action, data, date)
 VALUES
-('127.0.0.1', 'post', '/', 'calculate', 'alpha', '2021-10-31'),
+('127.0.0.1', 'post', '/', 'calculate', 'alpha', '2021-10-30'),
 ('127.0.0.1', 'post', '/', 'calculate', 'prime', '2021-10-31');
 
